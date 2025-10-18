0bf2d9b334df8e4f1abb947f95a8e4ad83ed70fd
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-18 09:14:07 +0300

    Added a New Project

---
+++ 4. List Manipulation Basics/Program.cs
@@ -0,0 +1,25 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+string[] command = Console.ReadLine().Split().ToArray();
+
+while (command[0] != "end")
+{
+    if (command[0] == "Add")
+    {
+        numbers.Add(int.Parse(command[1]));
+    }
+    if (command[0] == "Remove")
+    {
+        numbers.Remove(int.Parse(command[1]));
+    }
+    if (command[0] == "RemoveAt")
+    {
+        numbers.RemoveAt(int.Parse(command[1]));
+    }
+    if (command[0] == "Insert")
+    {
+        numbers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+    }
+
+    command = Console.ReadLine().Split().ToArray();
+}
+Console.WriteLine(string.Join(" ", numbers));
